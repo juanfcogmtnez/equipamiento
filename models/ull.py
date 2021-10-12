6b92b0c084714ac39a80c4699c4ce31c2b52d89b
root <juanfcogmtnez@gmail.com>
2021-10-12 17:53:50 +0200

    202110121752

--- models/ull.py
+++ models/ull.py
@@ -6,15 +6,10 @@ from odoo import fields, models, api
 class Ull(models.Model):
 	_name = "ull"
 	name = fields.Char(string="Nombre")
-	equipamiento = fields.Many2one(
-		comodel_name = "equipamiento",
-		string = "Equipamiento"
-	)
-	
-	active = fields.Boolean(string = 'Activo',default=True)
-	equipamiento_es = fields.Char(string="Nombre",related='equipamiento.name_es')
-	cantidad = fields.Integer(string="Cantidad",default="1")
-	ull_ids = fields.Char(string="ULL", related='equipamiento.ull_ids')
-	tipo = fields.Char(string="Tipo",related='equipamiento.tipo')
-	sub_tipo = fields.Char(string="Sub-tipo",related='equipamiento.sub_tipo')
-	tipo_arq = fields.Char(string="Tipo Arq",related='equipamiento.tipo_arq')
+	descripcion = fields.Text()
+	active = fields.Boolean(string="Activo",default="1")	
+	equipamiento_ids = fields.One2many(
+		comodel_name='equipamiento',
+		inverse_name = 'name',
+		string = "Equipamiento",
+		)
