1ace2b979fc70dd07922f3c87f1acb524ee6b2f9
root <juanfcogmtnez@gmail.com>
2021-10-13 09:35:24 +0200

    202110130935

--- models/sub_ull.py
+++ models/sub_ull.py
@@ -8,7 +8,7 @@ class Sub_ull(models.Model):
 	name = fields.Char(string="Nombre")
 	descripcion = fields.Text()
 	active = fields.Boolean(string="Activo",default="1")	
-	equipamiento_sub_ull_ids = fields.One2many(
+	equipamiento_sub_ull_ids = fields.Many2many(
 		comodel_name='equipamiento',
 		inverse_name = 'sub_ull_ids',
 		string = "Equipamiento",
--- models/sub_ull_2.py
+++ models/sub_ull_2.py
@@ -8,7 +8,7 @@ class Sub_ull_2(models.Model):
 	name = fields.Char(string="Nombre")
 	descripcion = fields.Text()
 	active = fields.Boolean(string="Activo",default="1")	
-	equipamiento_sub_ull_2_ids = fields.One2many(
+	equipamiento_sub_ull_2_ids = fields.Many2many(
 		comodel_name='equipamiento',
 		inverse_name = 'sub_ull_2_ids',
 		string = "Equipamiento",
