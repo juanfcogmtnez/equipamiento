5963b62df61c8993dc47696f9fd1a63f5a984be7
juanfcogmtnez <juanfcogmtnez@gmail.com>
2021-10-14 14:19:01 +0200

    202110141418

--- models/equipamiento.py
+++ models/equipamiento.py
@@ -24,7 +24,7 @@ class Equipamiento(models.Model):
 		string = 'Sub-ULL-2'
 	)
 	item_code = fields.Char(string="item-code")
-	
+
 	descripcion_es = fields.Text(string='Descripción')
 	descripcion_fr = fields.Text(string='Description')
 	descripcion_en = fields.Text(string='Description')
@@ -40,3 +40,28 @@ class Equipamiento(models.Model):
 	es_datasheet_en = fields.Boolean(string = 'Datasheet')
 	datasheet_en = fields.Binary(string="Datasheet")
 	datasheet_filename_en = fields.Char(string="File name")
+	car_alto = fields.Float(string="Alto(cm)")
+	car_ancho = fields.Float(string="Ancho(cm)")
+	car_largo = fields.Float(string="Largo(cm)")
+	car_peso = fields.Float(string="Peso(kg)")
+	superficie = fields.Float(string="Superficie (cm2)",compute="_sup_basic",stored=True)
+	@api.onchange('car_ancho','car_largo')
+	def _sup_basic(self):
+		cm_superficie = self.car_ancho * self.car_largo
+		self.superficie = cm_superficie/10000
+	ubicacion = fields.Selection(selection=[
+	('suelo','suelo'),
+	('mural','mural'),
+	('sobremesa','sobremesa'),
+	],
+	string = 'Ubicación predeterminada'
+	)
+	movilidad = fields.Boolean(string="¿Tiene el equipo una ubicación fija?", default=True)
+	fijacion = fields.Boolean(string="¿Necesita el equipo alguna fijación especial?", default=False)
+	estructura = fields.Boolean(string="¿Necesita el equipamiento de cambios constructivos en la estructura?", default=False)
+	conex_agua = fields.Boolean(string="¿Necesita el equipo conexión a circuito de agua corriente?", default=False)
+	conex_tta = fields.Boolean(string="¿Necesita el equipo conexión a circuito de agua especial?", default=False)
+	conex_elect = fields.Boolean(string="¿Necesita el equipo conexión a circuito eléctrico corriente?", default=False)
+ 	#conex_ups = fields.Boolean(string="¿Necesita el equipo conexión a circuito eléctrico de emergencia?", default=False)
+	conex_com = fields.Boolean(string="¿Necesita el equipo conexión a red de comunicaciones?", default=False)
+	conex_ups = fields.Boolean(string="¿Necesita el equipo conexión a circuito eléctrico de emergencia?", default=False)
